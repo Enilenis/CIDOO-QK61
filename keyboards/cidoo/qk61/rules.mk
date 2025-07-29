be468670051fa35ea383114c5f8caaff8f219a10
Enilenis <53215991+Enilenis@users.noreply.github.com>
2025-07-30 01:54:17 +0400

    Library upgrades and cleanup

--- keyboards/cidoo/qk61/rules.mk
+++ keyboards/cidoo/qk61/rules.mk
@@ -1,13 +1,12 @@
 # Build Options
-#   change yes to no to disable
-#
+# change yes to no to disable
+
 # Board: it should exist either in <chibios>/os/hal/boards/
 #  or <this_dir>/boards
-SRC += keyboard.c
-
 BOARD = FS026
 EEPROM_CUSTOM = custom
 NO_USB_STARTUP_CHECK = yes
 BLUETOOTH_CUSTOM = yes
 
+SRC += keyboard.c
 DEBOUNCE_TYPE = asym_eager_defer_pk
